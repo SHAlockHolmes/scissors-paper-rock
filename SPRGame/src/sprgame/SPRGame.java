d85bdc65863ef5acb26851e59d9edd15adc8c3e9
Cheryl A Fernando <128177155+SHAlockHolmes@users.noreply.github.com>
2023-07-28 18:10:20 +0530

    Update SPRGame.java

--- SPRGame/src/sprgame/SPRGame.java
+++ SPRGame/src/sprgame/SPRGame.java
@@ -14,14 +14,7 @@ public class SPRGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int r;
-        //r=(int)((Math.random() * (3 - 0)) + 0);
-        for(int i=0; i<15; i++)
-        {
-            r=(int)((Math.random() * (3 - 0)) + 0);
-            System.out.println("r: "+r);// TODO code application logic here
-        }
-
+        
     }
     
 }
